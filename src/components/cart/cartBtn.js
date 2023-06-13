8eb912abacc05c7e715cc4ec84f41fc1d99be47d
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-14 04:04:39 +0530

    cart button component in cart folder

---
+++ src/components/cart/cartBtn.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import { Button} from "react-bootstrap";
+
+const CartBtn=() =>{
+    return(
+        <Fragment>
+             <Button size="md">CART</Button>
+        </Fragment>
+    )
+}
+
+export default CartBtn;
