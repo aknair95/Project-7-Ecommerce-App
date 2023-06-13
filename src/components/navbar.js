d7ad9f6db001931ae4e9b93876a04f522d8d170f
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-14 04:04:57 +0530

    navbar component updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,19 @@
 import { Fragment } from "react";
 import { Container, Navbar } from "react-bootstrap";
+import CartCounter from "./cart/cartCounter";
+import CartBtn from "./cart/cartBtn";
 
 
 const NavbarTop=() =>{
     return(
         <Fragment>
-            <Navbar bg="black" expand="sm" variant="dark">
+            <Navbar bg="black" expand="sm" variant="dark" style={{height:"55px"}}>
                 <Container>
-                    <Navbar.Brand style={{fontFamily:"times-new-roman"}} className="m p-1">
-                        <h2 className="text">HOME STORE ABOUT</h2>
+                    <Navbar.Brand style={{fontFamily:"times-new-roman",marginLeft:"560px"}}>
+                        <h4>HOME STORE ABOUT</h4>
                     </Navbar.Brand>
+                    <CartBtn/>
+                    <CartCounter/>
                 </Container>
             </Navbar>
         </Fragment>
