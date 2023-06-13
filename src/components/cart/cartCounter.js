cac08e509039e754c39e28b781256ef8cf05ca35
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-14 04:04:20 +0530

    counter component for cart item counting

---
+++ src/components/cart/cartCounter.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const CartCounter=() =>{
+    return(
+        <Fragment>
+            <h5 style={{color:"blue"}}>0</h5>
+        </Fragment>
+    )
+}
+
+export default CartCounter;
