eb7758715649079f94f3aac8809fa8b809a65e7a
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-14 04:58:26 +0530

    music heading added to heading component

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,14 +1,25 @@
+import { Fragment } from "react";
 import { Container, Navbar } from "react-bootstrap";
 
 const HeadingBar=() =>{
     return(
-    <Navbar expand="sm" variant="dark" style={{height:"250px",backgroundColor:"#777777ff"}}>
-        <Container>
-            <Navbar.Brand style={{fontSize:"140px",marginLeft:"300px",marginBottom:"80px",fontFamily:"times-new-roman"}}>
-                The Generics
-            </Navbar.Brand>
-        </Container>
-    </Navbar>
+        <Fragment>
+            <Navbar expand="sm" variant="dark" style={{height:"250px",backgroundColor:"#777777ff"}}>
+                <Container>
+                    <Navbar.Brand style={{fontSize:"140px",marginLeft:"280px",marginBottom:"80px",fontFamily:"times-new-roman"}}>
+                    The Generics
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Navbar>
+                <Container>
+                    <Navbar.Brand style={{fontSize:"35px",marginLeft:"580px",fontFamily:"cursive",fontWeight:"bolder"}}>
+                        MUSIC
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </Fragment>
+    
     )
 }
 
