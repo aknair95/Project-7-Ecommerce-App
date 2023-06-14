b86f023c53b728b2bae968d52be256d8c07073ec
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-14 06:26:05 +0530

    album  component for showing products as tiles

---
+++ src/components/albumTile.js
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
+const Album=(props) =>{
+    return(
+        <Fragment>
+            <Container className="mt-3">
+                <Row>
+                    <Col xs={4}>
+                        <Card>
+                            <Card.Header className="text-center" style={{fontWeight:"bold",fontFamily:"times-new-roman",fontSize:"26px"}}>{props.albumDetails1.title}</Card.Header>
+                            <Card.Body style={{marginLeft:"38px"}}><img src={props.albumDetails1.imageUrl}/></Card.Body>
+                            <Card.Footer className="text-center">
+                                <h5 style={{fontWeight:"bold"}}>{`Rs ${props.albumDetails1.price}`}</h5>
+                            </Card.Footer>
+                            <Button style={{fontWeight:"bold",float:"right"}}>ADD TO CART</Button>
+                        </Card>
+                    </Col>
+                    <Col xs={4} style={{marginLeft:"26rem"}}>
+                        <Card>
+                            <Card.Header className="text-center" style={{fontWeight:"bold",fontFamily:"times-new-roman",fontSize:"26px"}}>{props.albumDetails2.title}</Card.Header>
+                            <Card.Body style={{marginLeft:"38px"}}><img src={props.albumDetails2.imageUrl}/></Card.Body>
+                            <Card.Footer className="text-center">
+                                <h5 style={{fontWeight:"bold"}}>{`Rs ${props.albumDetails2.price}`}</h5>
+                            </Card.Footer>
+                            <Button style={{fontWeight:"bold",float:"right" }}>ADD TO CART</Button>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>     
+        </Fragment>
+    )
+}
+
+export default Album;
