cdf15e23987076126df61889d199a0d578207ee5
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-16 17:51:05 +0530

    cart items counter works

--- src/components/cart/cartCounter.js
+++ src/components/cart/cartCounter.js
@@ -1,9 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import CartContext from "../../store/cartContext";
 
 const CartCounter=() =>{
+    const cartCtx=useContext(CartContext);
+    const totalCartQty=cartCtx.cartItems.reduce((count,element) =>{
+        return count+element.qty;
+    },0);
+    
     return(
         <Fragment>
-            <h5 style={{color:"blue",marginLeft:"5px"}}>0</h5>
+            <h5 style={{color:"blue",marginLeft:"5px"}}>{totalCartQty}</h5>
         </Fragment>
     )
 }
