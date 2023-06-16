e950237e36aa1c204223268ceba09bfc864c6706
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-16 20:15:26 +0530

    horizontal line added music & generics

--- src/components/heading.js
+++ src/components/heading.js
@@ -7,14 +7,14 @@ const HeadingBar=() =>{
             <Navbar expand="sm" variant="dark" style={{height:"250px",backgroundColor:"#777777ff"}}>
                 <Container>
                     <Navbar.Brand style={{fontSize:"140px",marginLeft:"280px",marginBottom:"80px",fontFamily:"times-new-roman"}}>
-                    The Generics
+                    The Generics<hr/>
                     </Navbar.Brand>
                 </Container>
             </Navbar>
             <Navbar>
                 <Container>
                     <Navbar.Brand style={{fontSize:"35px",marginLeft:"580px",fontFamily:"cursive",fontWeight:"bolder"}}>
-                        MUSIC
+                        MUSIC<hr/>
                     </Navbar.Brand>
                 </Container>
             </Navbar>
