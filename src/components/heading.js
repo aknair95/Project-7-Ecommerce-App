40672c7be931604e6bf671458a0fb55086328a80
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-21 04:34:14 +0530

    heading component updated for store & about page

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Container, Navbar } from "react-bootstrap";
 
-const HeadingBar=() =>{
+const HeadingBar=(props) =>{
     return(
         <Fragment>
             <Navbar expand="sm" variant="dark" style={{height:"250px",backgroundColor:"#777777ff"}}>
@@ -14,7 +14,7 @@ const HeadingBar=() =>{
             <Navbar>
                 <Container>
                     <Navbar.Brand style={{fontSize:"35px",marginLeft:"580px",fontFamily:"cursive",fontWeight:"bolder"}}>
-                        MUSIC<hr/>
+                        {props.aboutPage ? "ABOUT" : "MUSIC"}<hr/>
                     </Navbar.Brand>
                 </Container>
             </Navbar>
