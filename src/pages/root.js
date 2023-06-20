da86b4bf254e63a636d9fea0cf790dd314265dfb
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-21 04:32:41 +0530

    root page component wrapping nav bar

---
+++ src/pages/root.js
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavigationBar from "../components/navbar";
+
+// to fix navbar in all pages with the content of current selected page.
+const RootPage=(props) =>{          
+    return(
+        <>
+            <NavigationBar setCartShow={props.showCart} setCartHide={props.hideCart} storePage={props.storePage} 
+            homePage={props.homePage} aboutPage={props.aboutPage}/>
+            <Outlet/>
+        </>
+    )
+}
+
+export default RootPage;
