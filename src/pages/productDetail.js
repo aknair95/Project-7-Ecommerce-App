7943f5cef1f90644c3aca072974bf86d9198242a
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-06-28 02:17:06 +0530

    dynamic title of selected product added

--- src/pages/productDetail.js
+++ src/pages/productDetail.js
@@ -2,16 +2,23 @@ import { Button, Card, Carousel, Container, Nav, Navbar } from "react-bootstrap"
 import { useParams } from "react-router-dom";
 import classes from "./productDetail.module.css";
 
-const ProductDetails=() =>{
+const ProductDetails=(props) =>{
     const param=useParams();
-
+    let index=-1;
+    let count=0;
+    props.albumDetails.map((item) =>{
+        if(item.id===param.Id){
+            return index=count;
+         } 
+         count++;
+        });
     return(
         <>
             <br/><br/><br/>
             <Card>
                 <Nav className={classes.albumHeading}>
                     <Navbar.Brand>
-                        <div>ALBUM 1</div>
+                        <div>{`ALBUM- ${props.albumDetails[index].title}`}</div>
                     </Navbar.Brand>
                 </Nav><br/><hr/>
                 <Container className={classes.container}>
@@ -19,19 +26,19 @@ const ProductDetails=() =>{
                         <Carousel.Item>
                             <img src="/images/album1/colors1.jpg" alt="color1" className={classes.image}/>
                             <Carousel.Caption>
-                                <h2>COLORS ONE</h2>
+                                <h2>{`${props.albumDetails[index].title} ONE`}</h2>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img src="/images/album1/colors2.jpg" alt="color2" className={classes.image}/>
                             <Carousel.Caption>
-                                <h2>COLORS TWO</h2>
+                                <h2>{`${props.albumDetails[index].title} TWO`}</h2>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img src="/images/album1/colors3.jpg" alt="color3" className={classes.image}/>
                             <Carousel.Caption>
-                                <h2>COLORS THREE</h2>
+                                <h2>{`${props.albumDetails[index].title} THREE`}</h2>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
