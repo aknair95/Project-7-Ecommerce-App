f5b6a8a8321b4d4135a24a28e54c5f70df0aaaf5
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-01 03:26:29 +0530

    context provider now wrapped app comp.

--- src/pages/root.js
+++ src/pages/root.js
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/navbar";
-import CartContextProvider from "../store/cartContextProvider";
 
 // to fix navbar in all pages with the content of current selected page.
 const RootPage=(props) =>{          
     return(
-        <CartContextProvider albumDetails={props.albumDetails}>
+        <>
             <NavigationBar setCartShow={props.showCart} setCartHide={props.hideCart} storePage={props.storePage} 
             homePage={props.homePage} aboutPage={props.aboutPage} contactUsPage={props.contactUsPage}/>
             <Outlet/>
-        </CartContextProvider>
+        </>
     )
 }
 
