56dfeadc8f89fa3c69105fe1431fb3b8d5c63d12
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-05 03:22:29 +0530

    signup comp. updated. http firebase request with axios

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,17 +1,29 @@
 import classes from "./signUp.module.css";
 import { Button,Container,Form,Navbar } from "react-bootstrap";
 import { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignUp=() =>{
     const emailRef=useRef();
     const passwordRef=useRef();
     const navigate=useNavigate();
 
-    const formSubmitHandler=(e) =>{
+    const signUpHandler= async (e) =>{
         e.preventDefault();
         const enteredEmail=emailRef.current.value;
         const enteredPassword=passwordRef.current.value; 
+
+        try{ 
+            await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAp5p5uu04Cw4kQK-AghTVrCMiAd4RXJL0",{
+                email: enteredEmail,
+                password: enteredPassword,
+                returnSecureToken: true
+             });
+            navigate("/login");
+            } catch(error){
+                alert("Please enter valid email & password(min length- 6 character)");
+            }
         
         emailRef.current.value="";
         passwordRef.current.value="";
@@ -34,12 +46,12 @@ const SignUp=() =>{
         <br/><hr/><br/>
         <Container className={classes.formContainer}>
             <h3 className="p-2">SIGN UP</h3>
-            <Form onSubmit={formSubmitHandler}>
+            <Form onSubmit={signUpHandler}>
                 <Form.Group className="p-3">                    
-                    <Form.Control type="email" placeholder="Enter Email ID" size="lg" ref={emailRef}/>
+                    <Form.Control type="email" placeholder="Enter Email ID" required size="lg" ref={emailRef}/>
                 </Form.Group>
                 <Form.Group className="p-3">                      
-                    <Form.Control type="password" placeholder="Enter Password" size="lg" ref={passwordRef} />
+                    <Form.Control type="password" placeholder="Enter Password" required size="lg" ref={passwordRef} />
                 </Form.Group>
                 <div className={classes.signUpBtn}>
                     <Button type="submit" size="lg">SIGN UP</Button>
